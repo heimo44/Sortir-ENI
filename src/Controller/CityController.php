5e5eb6593463fb96292af1a73b26a6adc0f1db07
heimo44 <heimo.chuong@gmail.com>
2024-10-31 14:19:06 +0100

    ajout ville

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Ville;
+use App\Form\SortieType;
+use App\Form\VilleType;
 use App\Service\CityApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -18,14 +22,15 @@ class CityController extends AbstractController
         $this->cityApiService = $cityApiService;
     }
 
-    // Annotation pour le rôle et la route sur la méthode d'action
     #[IsGranted('ROLE_ADMIN')]
     #[Route('/cities', name: 'cities_list', methods: ['GET'])]
-    public function list(): Response
+    public function list(Request $request): Response
     {
-        $cities = $this->cityApiService->getCities();
-
-        return $this->redirectToRoute('cities_list');
+        $ville = new Ville();
+        $form = $this->createForm(VilleType::class, $ville);
+        $form->handleRequest($request);
+        $cities = $this->cityApiService->getPostalCodeByCityName("Nantes");
+        return $this->render('city/villes.html.twig', ['cities' => $cities, 'form' => $form->createView()]);
     }
 }
 
--- src/Service/CityApiService.php
+++ src/Service/CityApiService.php
@@ -2,6 +2,8 @@
 
 namespace App\Service;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
@@ -12,15 +14,16 @@ class CityApiService
 {
     private HttpClientInterface $client;
 
-    public function __construct(HttpClientInterface $client)
+
+    public function __construct(HttpClientInterface $client, RouterInterface $router)
     {
         $this->client = $client;
+        $this->router = $router;
     }
-
     public function getCities(): array
     {
         try {
-            $response = $this->client->request('GET', 'https://geo.api.gouv.fr/communes?nom=Paris', [
+            $response = $this->client->request('GET', 'https://geo.api.gouv.fr/communes', [
                 'verify_peer' => false,  // Désactive la vérification SSL pour l'environnement de dev
             ]);
 
@@ -28,6 +31,43 @@ class CityApiService
         } catch (TransportExceptionInterface | ClientExceptionInterface | RedirectionExceptionInterface | ServerExceptionInterface $e) {
             throw new \Exception('Erreur lors de la récupération des données : ' . $e->getMessage());
         }
+    }
+
+    public function getPostalCodeByCityName(string $cityName): array
+    {
+        return $this->searchByCommune($cityName);
+    }
+
+    public function getCityByPostalCode(string $postalCode): array
+    {
+        try{
+            $response = $this->client->request('GET', "https://geo.api.gouv.fr/communes/$postalCode", [
+                'verify_peer' => false,
+            ]);
+            return $response->toArray();
+        } catch(TransportExceptionInterface | ClientExceptionInterface | RedirectionExceptionInterface | ServerExceptionInterface $e){
+            $this->redirectToMain();
+            return [$e->getMessage()];
+        }
+    }
+
+    private function searchByCommune(string $commune): array
+    {
+        $c = ucfirst(strtolower($commune));
+        try{
+            $response = $this->client->request('GET', "https://geo.api.gouv.fr/communes?nom=$c", [
+                'verify_peer' => false,
+            ]);
+            return $response->toArray();
+        } catch(TransportExceptionInterface | ClientExceptionInterface | RedirectionExceptionInterface | ServerExceptionInterface $e){
+            $this->redirectToMain();
+            return [$e->getMessage()];
+        }
+    }
+
+    public function redirectToMain(): RedirectResponse
+    {
+        return new RedirectResponse($this->router->generate('main_accueil'));
     }
 }
 
